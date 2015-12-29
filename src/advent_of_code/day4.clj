7706eea293942400892c9fcfe9bd07ee4ea4f460
Aditya <blackleopard92@gmail.com>
2015-12-29 17:57:09 +0530

    day 4 done. day 5 begin

---
+++ src/advent_of_code/day4.clj
@@ -0,0 +1,19 @@
+(ns advent-of-code.day4
+  (:use clojure.test)
+  (:use clojure.pprint)
+  (:require [clojure.string :as str]
+            [clojure.set :as s]
+            [pandect.algo.md5 :refer :all]))
+
+;; ideal stocking buffer
+(defn min-int-suffix
+  [prefix]
+  (->> (repeat prefix)
+       (map-indexed #(str %2 %1))
+       (map (juxt identity md5))
+       (filter #(.startsWith (second %) "00000"))
+       (first)
+       ))
+
+(deftest ans1
+  (is (= 609043 (min-int-suffix "abcde"))))
