ed6d647cb8ef6c9067777f16331ba7def4ca492c
Aditya <blackleopard92@gmail.com>
2015-12-31 01:25:19 +0530

    bunch of refactorings to make code nicer

--- src/advent_of_code/day6.clj
+++ src/advent_of_code/day6.clj
@@ -5,26 +5,22 @@
             [clojure.set :as s]))
 
 ;;turn on the lights
-
-
-(defn turn-on  [bulb-state] true)
-(defn turn-off [bulb-state] false)
-(defn toggle [bulb-state] (not (or bulb-state false)))
-
-(defn cmd->str1
+;; for part 1 soln
+(defn cmd->action1
   [cmd-type]
   (case cmd-type
-    "toggle" toggle
-    "turn on" turn-on
-    "turn off" turn-off
+    "toggle" #(not (or % false))
+    "turn on" (constantly true)
+    "turn off" (constantly false)
     identity))
 
-(defn cmd->str2
+;; part 2 soln
+(defn cmd->action2
     [cmd-type]
     (case cmd-type
-      "toggle" (fn [state] (+ 2 (or state 0)))
-      "turn on" (fn [state] (inc (or state 0)))
-      "turn off" (fn [state] (max 0 (dec (or state 0))))
+      "toggle" #(+ 2 (or % 0))
+      "turn on" #(+ 1 (or % 0))
+      "turn off" #(max 0 (dec (or % 0)))
       identity))
 
 ;; command parsing
@@ -32,43 +28,54 @@
   [cmd-string]
   (let [[blah main-cmd from-x from-y to-x to-y]
         (first (re-seq #"(.+)\s(\d{1,3}),(\d{1,3}) through (\d{1,3}),(\d{1,3})" cmd-string))]
-    {:main-cmd (cmd->str2 main-cmd)
+    {:main-cmd main-cmd
      :from [(Integer/parseInt from-x) (Integer/parseInt from-y)]
      :to [(inc (Integer/parseInt to-x)) (inc (Integer/parseInt to-y))]})
   )
 
+(defn coord-stream
+  "generate a stream of [x y] coords for the given ranges"
+  [[from-x from-y] [to-x to-y]]
+  (for [x (range from-x to-x) y (range from-y to-y)] [x y]))
+
 ;; bulb map updator. f is applied to bulb-state returns new state
 (defn update-bulb
-  [bulb-map x y f]
-  (update-in bulb-map [x y] f))
+  [bulb-map coord f]
+  (update-in bulb-map coord f))
+
 ;; applies the f val and update the map with the results.
 (defn for-each-bulb
-  [bulb-map [from-x from-y] [to-x to-y] f]
-  (reduce
-   #(update-bulb %1 (first %2) (second %2) f) bulb-map
-   (for [x (range from-x to-x) y (range from-y to-y)] [x y]))
+  [bulb-map from to f]
+  (reduce #(update-bulb %1 %2 f) bulb-map (coord-stream from to))
   )
 
 (defn num-lit-lights
   [bulb-map]
   (reduce (fn [count coords]
-            (if (get-in bulb-map coords) (inc count) count)) 0
-          (for [x (range 0 1000) y (range 0 1000)] [x y]))
-  )
+            (if (get-in bulb-map coords) (inc count) count))
+          0
+          (coord-stream [0 0] [1000 1000])))
 
 (defn total-brightness
   [bulb-map]
   (reduce (fn [count coords]
             (if-let [brightness (get-in bulb-map coords)]
-              (+ count brightness) count)) 0
-          (for [x (range 0 1000) y (range 0 1000)] [x y])
-          ))
+              (+ count brightness) count))
+          0
+          (coord-stream [0 0] [1000 1000])))
+
+
+(defn resolve-actions
+  "returns a function which updates a command with the correct action to take on bulb map"
+  [f]
+  (fn [a-cmd] (update a-cmd :main-cmd f)))
 
 (defn solve1
   []
   (num-lit-lights (with-open [rdr (clojure.java.io/reader "resources/day6-input.txt")]
                     (->> (line-seq rdr)
                          (map str->cmd)
+                         (map (resolve-actions cmd->action1))
                          (reduce (fn [bulb-map {:keys [main-cmd to from]}]
                                    (for-each-bulb bulb-map from to main-cmd)) {}))
                     )))
@@ -78,6 +85,7 @@
    (with-open [rdr (clojure.java.io/reader "resources/day6-input.txt")]
      (->> (line-seq rdr)
           (map str->cmd)
+          (map (resolve-actions cmd->action2))
           (reduce (fn [bulb-map {:keys [main-cmd to from]}]
                     (for-each-bulb bulb-map from to main-cmd)) {})
           )))
